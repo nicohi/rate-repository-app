4f7d5fa590825bf97ae30bf20cd064d378ff9869
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-07-25 21:41:21 +0300

    Refactor

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,5 @@
 import { FlatList, View, StyleSheet } from 'react-native';
-import { useState, useEffect } from 'react';
+import useRepositories from '../hooks/useRepositories';
 
 import RepositoryItem from './RepositoryItem';
 
@@ -13,26 +13,12 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState();
-
-  const fetchRepositories = async () => {
-    const response = await fetch('http://192.168.88.139:5000/api/repositories');
-    const json = await response.json();
-
-    console.log(json);
-
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
+  const { repositories } = useRepositories();
 
   // Get the nodes from the edges array
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
-
 
   return (
     <FlatList
