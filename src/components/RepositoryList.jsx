8fb31812ca5b0180222e93ca354972b128abb12f
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-07-25 22:23:04 +0300

    Complete 10.11

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -15,7 +15,6 @@ const ItemSeparator = () => <View style={styles.separator} />;
 const RepositoryList = () => {
   const { repositories } = useRepositories();
 
-  // Get the nodes from the edges array
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,25 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
 
-  const fetchRepositories = async () => {
-    setLoading(true);
+  const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES,
+                                            {
+                                              fetchPolicy: 'cache-and-network',
+                                              onCompleted: () => setRepositories(data.repositories),
+                                            }
+                                           );
 
-    // Replace the IP address part with your own IP address!
-    const response = await fetch('http://192.168.88.139:5000/api/repositories');
-    const json = await response.json();
 
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories, error, loading, refetch };
 };
 
 export default useRepositories;
