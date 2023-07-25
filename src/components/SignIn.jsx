3dea0eb747c8870a26c0f0024b7ea4fe59910432
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-07-25 20:32:12 +0300

    Complete 10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,65 @@
+import { View, Pressable, StyleSheet } from 'react-native';
+import FormikTextInput from './FormikTextInput';
+import { Formik } from 'formik';
+
 import Text from './Text';
+import theme from '../theme';
+
+const onSubmit = (values) => {
+  console.log('username:', values.username);
+  console.log('password:', values.password);
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  return (
+    <Formik
+      initialValues={{
+        username: '',
+        password: '',
+      }}
+      onSubmit={onSubmit}
+    >
+    {({ handleSubmit }) => (
+      <View style={styles.vccontainer}>
+        <FormikTextInput style={styles.item} name="username" placeholder="Username" />
+        <FormikTextInput style={styles.item} name="password" placeholder="Password" secureTextEntry={true} />
+        <Pressable onPress={handleSubmit}>
+          <Text style={styles.button}>Sign in</Text>
+        </Pressable>
+      </View>
+    )}
+    </Formik>
+  )
 };
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    marginVertical: 2,
+    marginHorizontal: 10,
+    backgroundColor: theme.colors.foreground,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderRadius: 5,
+    alignSelf: 'stretch',
+  },
+  button: {
+    padding: 10,
+    marginVertical: 2,
+    marginHorizontal: 10,
+    backgroundColor: theme.colors.primary,
+    color: theme.colors.textSecondary,
+    borderRadius: 5,
+    alignSelf: 'stretch',
+  },
+  vccontainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: 2,
+    alignItems: 'stretch',
+    backgroundColor: theme.colors.foreground,
+  },
+});
 
 export default SignIn;
